4921e79cebc702245f12246ac8b155a3a3b5f500
Cumi Oyemike <cuoyemike@gmail.com>
2023-09-16 04:41:52 +0200

    fixes

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,7 +2,7 @@
 def matrix_shape(mat):
     """ Calculates the shpe of a matrix """
     shape = []
-    while isinstance(matrix, list):
-        matrix_shape.append(len(matrix))
-        matrix = matrix[0]
+    while isinstance(mat, list):
+        matrix_shape.append(len(mat))
+        mat = mat[0]
     return matrix_shape
