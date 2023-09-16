7dec9594d41df0f3874f57442c8c3fa68d8ac7ee
CtripleU <cuoyemike@gmail.com>
2023-09-15 19:30:49 -0700

    matrix size

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def matrix_shape(mat):
+    """ Calculates the shpe of a matrix """
+    shape = []
+    while isinstance(matrix, list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
