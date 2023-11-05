d84f9abfa98373aef5b57b91d95ccc8be1a41c2e
Cumi Oyemike <cuoyemike@gmail.com>
2023-11-05 23:17:15 +0200

    fixes

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,59 +8,90 @@ given various hypothetical probabilities of developing severe side effects.
 import numpy as np
 
 
+# def likelihood(x, n, P):
+#   """Calculates the likelihood of obtaining the data given various hypothetical probabilities of developing severe side effects.
+
+#   Args:
+#     x: The number of patients that develop severe side effects.
+#     n: The total number of patients observed.
+#     P: A 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects.
+
+#   Returns:
+#     A 1D numpy.ndarray containing the likelihood of obtaining the data, x and n, for each probability in P, respectively.
+#   """
+
+#   # Check if n is a positive integer.
+#   if not isinstance(n, int) or n <= 0:
+#     raise ValueError("n must be a positive integer.")
+
+#   # Check if x is an integer that is greater than or equal to 0.
+
+  
+# if
+ 
+# not
+ 
+# isinstance(x, int) or x < 0:
+#     raise ValueError("x must be an integer that is greater than or equal to 0.")
+
+#   # Check if x is not greater than n.
+
+  
+# if x > n:
+#     raise ValueError("x cannot be greater than n.")
+
+#   # Check if P is a 1D numpy.ndarray.
+
+  
+# if
+ 
+# not
+ 
+# isinstance(P, np.ndarray) or P.ndim != 1:
+#     raise TypeError("P must be a 1D numpy.ndarray.")
+
+#   # Check if all values in P are in the range [0, 1].
+
+  
+# if np.any(P < 0) or np.any(P > 1):
+#     raise ValueError("All values in P must be in the range [0, 1]")
+
+#   # Calculate the likelihood for each probability in P.
+#   likelihoods = np.zeros_like(P)
+#   for i in
+ 
+# range(len(P)):
+#     likelihoods[i] = np.binom.pmf(x, n, P[i])
+
+#   return likelihoods
+
+
 def likelihood(x, n, P):
-  """Calculates the likelihood of obtaining the data given various hypothetical probabilities of developing severe side effects.
+    # Check if n is a positive integer
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    
+    # Check if x is an integer greater than or equal to 0
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    
+    # Check if x is greater than n
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    
+    # Check if P is a 1D numpy array
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    
+    # Check if all values in P are in the range [0, 1]
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    
+    # Calculate the likelihood for each probability in P
+    likelihoods = np.array([np.math.comb(n, x) * (p**x) * ((1-p)**(n-x)) for p in P])
+    
+    return likelihoods
 
-  Args:
-    x: The number of patients that develop severe side effects.
-    n: The total number of patients observed.
-    P: A 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects.
-
-  Returns:
-    A 1D numpy.ndarray containing the likelihood of obtaining the data, x and n, for each probability in P, respectively.
-  """
-
-  # Check if n is a positive integer.
-  if not isinstance(n, int) or n <= 0:
-    raise ValueError("n must be a positive integer.")
-
-  # Check if x is an integer that is greater than or equal to 0.
-
-  
-if
- 
-not
- 
-isinstance(x, int) or x < 0:
-    raise ValueError("x must be an integer that is greater than or equal to 0.")
-
-  # Check if x is not greater than n.
-
-  
-if x > n:
-    raise ValueError("x cannot be greater than n.")
-
-  # Check if P is a 1D numpy.ndarray.
-
-  
-if
- 
-not
- 
-isinstance(P, np.ndarray) or P.ndim != 1:
-    raise TypeError("P must be a 1D numpy.ndarray.")
-
-  # Check if all values in P are in the range [0, 1].
-
-  
-if np.any(P < 0) or np.any(P > 1):
-    raise ValueError("All values in P must be in the range [0, 1]")
-
-  # Calculate the likelihood for each probability in P.
-  likelihoods = np.zeros_like(P)
-  for i in
- 
-range(len(P)):
-    likelihoods[i] = np.binom.pmf(x, n, P[i])
-
-  return likelihoods
+if __name__ == '__main__':
+    P = np.linspace(0, 1, 11)
+    print(likelihood(26, 130, P))
