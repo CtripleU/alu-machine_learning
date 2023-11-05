a5369d37bb347e502f11952ebd17ffcaa0ef3f96
Cumi Oyemike <cuoyemike@gmail.com>
2023-11-05 23:48:49 +0200

    yet another fix

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -9,13 +9,18 @@ import numpy as np
 
 
 def likelihood(x, n, P):
-    """Calculates the likelihood of obtaining the data given various hypothetical probabilities of developing severe side effects.
+    """Calculates the likelihood of obtaining the data given various hypothetical
+       probabilitiesnof developing severe side effects.
+
     Args:
         x: The number of patients that develop severe side effects.
         n: The total number of patients observed.
-        P: A 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects.
+        P: A 1D numpy.ndarray containing the various hypothetical probabilities 
+           of developing severe side effects.
+
     Returns:
-        A 1D numpy.ndarray containing the likelihood of obtaining the data, x and n, for each probability in P, respectively.
+        A 1D numpy.ndarray containing the likelihood of obtaining the data, x and n, 
+        for each probability in P, respectively.
     """
 
     # Check if n is a positive integer.
@@ -33,24 +38,25 @@ def likelihood(x, n, P):
     # Check if x is an integer greater than or equal to 0
     if not isinstance(x, int) or x < 0:
         raise ValueError("x must be an integer that is greater than or equal to 0")
-    
+
     # Check if x is greater than n
     if x > n:
         raise ValueError("x cannot be greater than n")
-    
+
     # Check if P is a 1D numpy array
     if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    
+
     # Check if all values in P are in the range [0, 1]
     for value in P:
         if value > 1 or value < 0:
             raise ValueError("All values in P must be in the range [0, 1]")
-    
+
     # # Check if P has values that start at 0.0 and end at 1.0 and is sorted
     # if P[0] != 0.0 or P[-1] != 1.0 or not np.all(np.diff(P) >= 0):
-    #     raise ValueError("P must have values starting at 0.0, ending at 1.0, and be sorted.")
-    
+    #     raise ValueError("P must have values starting at 0.0, ending at
+    #     1.0, and be sorted.")
+
     # Calculate the likelihood for each probability in P
     factorial = np.math.factorial
     fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
