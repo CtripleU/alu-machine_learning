3ff6900702263afeebe9ef8973a39cfb2fbc1095
Cumi Oyemike <cuoyemike@gmail.com>
2024-03-16 00:18:08 +0200

    correct 2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-
-
-""" Return list of ships"""
-
-import requests
+""" Get requests location from Github API"""
 import sys
+import requests
 import time
 
 
-if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
+if __name__ == '__main__':
 
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print("Reset in {} min".format(diff))
-        # get remaining rate
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    url = sys.argv[1]
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        print(response.json()['location'])
+    if response.status_code == 404:
+        print('Not found')
+    if response.status_code == 403:
+        limit = int(response.headers['X-Ratelimit-Reset'])
+        start = int(time.time())
+        elapsed = int((limit - start) / 60)
+        print('Reset in {} min'.format(int(elapsed)))
