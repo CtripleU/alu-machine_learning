9dd202fa1729ace931687b562251006f25432fcc
Cumi Oyemike <cuoyemike@gmail.com>
2024-05-19 23:41:40 +0200

    test to see if this woks

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,105 +1,96 @@
 #!/usr/bin/env python3
-"""
-NST - Initialize
-"""
+'''class NST that performs neural style transfer'''
 import numpy as np
 import tensorflow as tf
-
+from tensorflow.keras.applications import VGG19
 
 class NST:
-    """class used to perform tasks for neural style transfer"""
+    '''class that performs neural style transfer'''
 
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
-                    'block4_conv1', 'block5_conv1']
+    style_layers = ['block1_conv1', 'block2_conv1',
+                    'block3_conv1', 'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """define and initialize variables"""
+        '''class initializer'''
+        if not isinstance(style_image, np.ndarray) or\
+                style_image.ndim != 3 or\
+                style_image.shape[-1] != 3:
+            raise TypeError(
+                'style_image must be a numpy.ndarray with shape (h, w, 3)')
+        if not isinstance(content_image, np.ndarray) or\
+                content_image.ndim != 3 or\
+                content_image.shape[-1] != 3:
+            raise TypeError(
+                'content_image must be a numpy.ndarray with shape (h, w, 3)')
+        if not isinstance(beta, (int, float)) or beta < 0:
+            raise TypeError('beta must be a non-negative number')
+        if not isinstance(alpha, (int, float)) or alpha < 0:
+            raise TypeError('alpha must be a non-negative number')
 
-        # After eager execution is enabled, operations are executed as they are
-        # defined and Tensor objects hold concrete values, which can be
-        # accessed as numpy.ndarray`s through the numpy() method.
         tf.enable_eager_execution()
 
-        err_1 = "style_image must be a numpy.ndarray with shape (h, w, 3)"
-        if not isinstance(style_image, np.ndarray):
-            raise TypeError(err_1)
-        if style_image.ndim != 3 or style_image.shape[-1] != 3:
-            raise TypeError(err_1)
-        err_2 = "content_image must be a numpy.ndarray with shape (h, w, 3)"
-        if not isinstance(content_image, np.ndarray):
-            raise TypeError(err_2)
-        if content_image.ndim != 3 or content_image.shape[-1] != 3:
-            raise TypeError(err_2)
-        if not isinstance(alpha, (int, float)) or alpha < 0:
-            raise TypeError("alpha must be a non-negative number")
-        if not isinstance(beta, (int, float)) or beta < 0:
-            raise TypeError("beta must be a non-negative number")
-
-        # Preprocessed style image
         self.style_image = self.scale_image(style_image)
-        # Preprocessed content image
         self.content_image = self.scale_image(content_image)
-        # Weight for content cost
         self.alpha = alpha
-        # Weight for style cost
         self.beta = beta
 
     @staticmethod
     def scale_image(image):
-        """
-        function that rescales an image such that its pixels values are
-        between 0 and 1 and its largest side is 512 pixels
-        """
+        '''Rescale an image's pixels to 0 and 1.
+        largest_image_size = 512 px
+        args: image (image to rescale)
+        return: rescaled image'''
+        if not isinstance(image, np.ndarray) or\
+                image.ndim != 3 or image.shape[-1] != 3:
+            raise TypeError(
+                'image must be a numpy.ndarray with shape (h, w, 3)')
+        else:
+            # get the initial dimensions
+            original_height, original_width, dim = image.shape
+            # calculate the new dimensions
 
-        err = "image must be a numpy.ndarray with shape (h, w, 3)"
-        if not isinstance(image, np.ndarray):
-            raise TypeError(err)
-        if image.ndim != 3 or image.shape[-1] != 3:
-            raise TypeError(err)
+            if original_height > original_width:
+                new_height = 512
+                new_width = int(original_width * (512/original_height))
+            else:
+                new_width = 512
+                new_height = int(original_height * (512 / original_width))
+            size = (new_height, new_width)
+            resized_image = tf.image.resize_bicubic(np.expand_dims(image,
+                                                                   axis=0),
+                                                    size)
 
-        # print("image:", type(image[0][0][0])) <-np.int, [0..255]
+            # clip the pixel values to [0, 1]
+            resized_image = resized_image / 255
+            resized_image = tf.clip_by_value(resized_image, 0.0, 1.0)
 
-        # Impose image rescaling such that largest side is 512 pixels
-        max_dim = 512
-        # print("max_dim:", type(max_dim)) <-int
-        long_dim = max(image.shape[:-1])
-        # print("long_dim:", type(long_dim)) <-int
-        scale = max_dim / long_dim
-        # print("scale:", scale, type(scale)) <-float
+            # confirm that the new shape is (1, hnew, w_new, 3)
+            resized_image = tf.ensure_shape(resized_image, [1, None, None, 3])
 
-        # Infer new_shape using the scale factor
-        # print("image.shape[:-1]:", image.shape[:-1],
-        #      type(image.shape[:-1][0])) <-int
-        # new_shape = scale * image.shape[:-1] <- TypeError:
-        # can't multiply sequence by non-int of type 'float'.
-        # use map() to convert scale * tuple element products (floats)
-        # to integers and recompose the tuple:
-        new_shape = tuple(map(lambda x: int(scale * x), image.shape[:-1]))
-        # print("new_shape:", new_shape, type(new_shape[0]))
-
-        # Convert np.ndarray with shape (h, w, 3) to shape (1, h, w, 3)
-        image = image[tf.newaxis, :]
-        # print(image)
-        # print(image.shape)
-
-        # Resize image using bicubic interpolation, concurrently
-        # converting np.ndarray to tf.tensor with shape (1, h_new, w_new, 3)
-        # In Google Colab (tf 2.0):
-        # image = tf.image.resize(image, (new_h, new_w), method='bicubic')
-        # With tf 1.2:
-        image = tf.image.resize_bicubic(image, new_shape)
-        # print("Before clipping:", image)
-        # print(image.shape)
-
-        # Normalize image pixels to range [0..1]:
-        image = image / 255
-        # print("image:", type(image[0][0][0])) <-np.float, [0..1]
-
-        # Since this is a float image, keep the pixel values between 0 and 1:
-        # clip data to the valid range for plt.imshow with RGB data
-        # ([0..1] for floats) <- required/requested by the script
-        image = tf.clip_by_value(image, clip_value_min=0, clip_value_max=1)
-        # print("After clipping:", image)
-
-        return image
+            return resized_image
+    def load_model(self):
+        '''loads a VGG19 model for neural transfer'''
+        # define the base_model
+        VGG19_model = VGG19(include_top=False, weights='imagenet')
+        # save model
+        VGG19_model.save('VGG19_base_model')
+        #Add customizable objects to model
+        #Here we are replacing any maxpooling layer
+        # in our model with average pooling 
+        custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
+        #load the model afresh with the customs
+        loaded_model = tf.keras.models.load_model("VGG19_base_model", custom_objects=custom_objects)
+        # define a list for outputs:
+        style_output = []
+        content_output = None
+        #check whether layer name is self.style_layers
+        for layer in loaded_model.layers:
+            if layer in self.style_layers:
+                style_output.append(layer.output)
+            if layer in self.content_layer:
+                content_output = (layer.output)
+            layer.trainable = False
+        output = style_output + content_output
+        model = tf.keras.models.Model(loaded_model.input, output)
+        return model
--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,79 +1,102 @@
 #!/usr/bin/env python3
-"""Neural Style Transfer Module"""
+'''class NST that performs neural style transfer'''
 import numpy as np
 import tensorflow as tf
 
 
 class NST:
-    """Performs tasks for neural style transfer
-    """
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
-                    'block4_conv1', 'block5_conv1']
+    '''class that performs neural style transfer'''
+
+    style_layers = ['block1_conv1', 'block2_conv1',
+                    'block3_conv1', 'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        if not isinstance(style_image, np.ndarray
-                          ) or len(style_image.shape
-                                   ) != 3 or style_image.shape[2] != 3:
+        '''class initializer'''
+        if not isinstance(style_image, np.ndarray) or\
+                style_image.ndim != 3 or\
+                style_image.shape[-1] != 3:
             raise TypeError(
-                "style_image must be a numpy.ndarray with shape (h, w, 3)")
-        if not isinstance(content_image, np.ndarray
-                          ) or len(content_image.shape
-                                   ) != 3 or content_image.shape[2] != 3:
+                'style_image must be a numpy.ndarray with shape (h, w, 3)')
+        if not isinstance(content_image, np.ndarray) or\
+                content_image.ndim != 3 or\
+                content_image.shape[-1] != 3:
             raise TypeError(
-                "content_image must be a numpy.ndarray with shape (h, w, 3)")
-        if not (isinstance(alpha, int) or isinstance(alpha, float
-                                                     )) or alpha < 0:
-            raise TypeError("alpha must be a non-negative number")
-        if not (isinstance(beta, int) or isinstance(beta, float)) or beta < 0:
-            raise TypeError("beta must be a non-negative number")
+                'content_image must be a numpy.ndarray with shape (h, w, 3)')
+        if not isinstance(beta, (int, float)) or beta < 0:
+            raise TypeError('beta must be a non-negative number')
+        if not isinstance(alpha, (int, float)) or alpha < 0:
+            raise TypeError('alpha must be a non-negative number')
+
+        tf.enable_eager_execution()
 
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
-        self.model = self.load_model()
+        self.load_model()
 
     @staticmethod
     def scale_image(image):
-        """Rescales an image such that its pixels values are between 0 and 1
-        """
-        if not isinstance(image, np.ndarray
-                          ) or len(image.shape) != 3 or image.shape[2] != 3:
+        '''Rescale an image's pixels to 0 and 1.
+        largest_image_size = 512 px
+        args: image (image to rescale)
+        return: rescaled image'''
+        if not isinstance(image, np.ndarray) or\
+                image.ndim != 3 or image.shape[-1] != 3:
             raise TypeError(
-                "image must be a numpy.ndarray with shape (h, w, 3)")
-
-        h, w, _ = image.shape
-        if h > w:
-            h_new = 512
-            w_new = w * h_new // h
+                'image must be a numpy.ndarray with shape (h, w, 3)')
         else:
-            w_new = 512
-            h_new = h * w_new // w
+            # get the initial dimensions
+            original_height, original_width, dim = image.shape
+            # calculate the new dimensions
 
-        scaled_image = tf.image.resize(image, tf.constant([h_new, w_new],
-                                                          dtype=tf.int32),
-                                       tf.image.ResizeMethod.BICUBIC)
-        scaled_image = tf.reshape(scaled_image, (1, h_new, w_new, 3))
-        scaled_image = tf.clip_by_value(scaled_image / 255, 0.0, 1.0)
+            if original_height > original_width:
+                new_height = 512
+                new_width = int(original_width * (512/original_height))
+            else:
+                new_width = 512
+                new_height = int(original_height * (512 / original_width))
+            size = (new_height, new_width)
+            resized_image = tf.image.resize_bicubic(np.expand_dims(image,
+                                                                   axis=0),
+                                                    size)
 
-        return scaled_image
+            # clip the pixel values to [0, 1]
+            resized_image = resized_image / 255
+            resized_image = tf.clip_by_value(resized_image, 0.0, 1.0)
+
+            # confirm that the new shape is (1, hnew, w_new, 3)
+            resized_image = tf.ensure_shape(resized_image, [1, None, None, 3])
+
+            return resized_image
 
     def load_model(self):
-        """Creates the model used to calculate cost
-        """
-        base_model = tf.keras.applications.VGG19(include_top=False)
-        base_model.trainable = False
-
+        '''loads a VGG19 model for neural transfer'''
+        # define the base_model
+        VGG19_model = tf.keras.applications.VGG19(
+            include_top=False, weights='imagenet')
+        # save model
+        VGG19_model.save('VGG19_base_model')
+        # Add customizable objects to model
+        # Here we are replacing any maxpooling layer
+        # in our model with average pooling
+        # load the model afresh with the customs
         custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
-        base_model.save('base_model')
-        base_model = tf.keras.models.load_model('base_model',
-                                                custom_objects=custom_objects)
+        loaded_model = tf.keras.models.load_model("VGG19_base_model.h5",
+                                                  custom_objects)
 
-        outputs = [base_model.get_layer(lyr
-                                        ).output for lyr in self.style_layers]
-        outputs.append(base_model.get_layer(self.content_layer).output)
-        model = tf.keras.Model(base_model.inputs, outputs)
-
+        # define a list for outputs:
+        style_output = []
+        content_output = None
+        # check whether layer name is self.style_layers
+        for layer in loaded_model.layers:
+            if layer.name in self.style_layers:
+                style_output.append(layer.output)
+            if layer.name in self.content_layer:
+                content_output = (layer.output)
+            layer.trainable = False
+        output = style_output + [content_output]
+        model = tf.keras.models.Model(loaded_model.input, output)
         self.model = model
-        return model
+        return self.model
