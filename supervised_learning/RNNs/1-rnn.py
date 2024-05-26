1db6e215d026ec09d4632cb382c7c29374c54b80
Cumi Oyemike <cuoyemike@gmail.com>
2024-05-26 23:28:00 +0200

    task 1

---
+++ supervised_learning/RNNs/1-rnn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+contains a function that performs forward propagation for a simple RNN
+"""
+
+def rnn(rnn_cell, X, h_0):
+    """
+    Performs forward propagation for a simple RNN
+    """
+    t, m, i = X.shape
+    _, h = h_0.shape
+
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros((t, m, rnn_cell.by.shape[1]))
+
+    H[0] = h_0
+
+    for step in range(t):
+        H[step + 1], Y[step] = rnn_cell.forward(H[step], X[step])
+
+    return H, Y
