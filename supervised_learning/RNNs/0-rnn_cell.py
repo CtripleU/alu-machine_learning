937878310d99fa47a0c97ebc89c67dea94954ae3
Cumi Oyemike <cuoyemike@gmail.com>
2024-05-26 23:24:04 +0200

    fix pycodestyle and documentation

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python3
+"""
+This module contains the RNNCell class
+"""
 
 import numpy as np
 
+
 class RNNCell:
+    """
+    Represents a cell of a simple RNN
+    """
     def __init__(self, i, h, o):
+        """
+        Class constructor
+        """
         self.Wh = np.random.normal(size=(i + h, h))
         self.Wy = np.random.normal(size=(h, o))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def softmax(self, x):
+        """
+        Softmax activation function
+        """
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum(axis=1, keepdims=True)
 
     def forward(self, h_prev, x_t):
+        """
+        Performs forward propagation for one time step
+        """
         concat = np.concatenate((h_prev, x_t), axis=1)
         h_next = np.tanh(np.dot(concat, self.Wh) + self.bh)
         y = self.softmax(np.dot(h_next, self.Wy) + self.by)
