a0f14988527d1890d42f54e57a10d2e28f141592
Cumi Oyemike <cuoyemike@gmail.com>
2024-07-07 21:03:00 +0200

    fix 8

--- unsupervised_learning/dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/dimensionality_reduction/8-tsne.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""Performs a t-SNE transformation"""
+"""
+Defines function that performs a t-SNE transformation
+"""
+
+
 import numpy as np
 pca = __import__('1-pca').pca
 P_affinities = __import__('4-P_affinities').P_affinities
@@ -10,42 +14,25 @@ cost = __import__('7-cost').cost
 def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     """
     Performs a t-SNE transformation
-    Args:
-        X: is a numpy.ndarray of shape (n, d) containing the dataset
-        to be transformed by t-SNE
-        ndims: is the new dimensional representation of X
-        idims: is the intermediate dimensional representation of X after PCA
-        perplexity: is the perplexity
-        iterations: is the number of iterations
-        lr: is the learning rate
-    For the first 100 iterations, early exaggeration with an exaggeration of 4
-    a(t) = 0.5 for the first 20 iterations and 0.8 thereafter
-    Returns:
-    Y, a numpy.ndarray of shape (n, ndim) containing the optimized
-    low dimensional transformation of X
+
+    parameters:
+        X [numpy.ndarray of shape (n, d)]:
+            containing the dataset to be transformed by t-SNE
+            n: the number of data points
+            d: the number of dimensions in each point
+        ndims [int]:
+            the new dimensional representation of X
+        idims [int]:
+            the intermediate dimensional representation of X after PCA
+        perplexity [float]:
+            perplexity that all Gaussian distributions should have
+        iterations [int]:
+            the number of iterations
+        lr [float]:
+            the learning rate
+
+    returns:
+        Y [numpy.ndarray of shape (n, ndim)]:
+            contatining the optimized low dimensional transformation of X
     """
-    X = pca(X, idims)
-    n, d = X.shape
-    P = P_affinities(X, perplexity=perplexity) * 4
-    Y = np.random.randn(n, ndims)
-    iY = np.zeros((n, ndims))
-
-    for i in range(iterations):
-        dY, Q = grads(Y, P)
-
-        if i <= 20:
-            momentum = 0.5
-        else:
-            momentum = 0.8
-
-        iY = momentum * iY - lr * dY
-        Y = Y + iY - np.tile(np.mean(Y, 0), (n, 1))
-
-        if (i + 1) != 0 and (i + 1) % 100 == 0:
-            C = cost(P, Q)
-            print("Cost at iteration {}: {}".format(i + 1, C))
-
-        if (i + 1) == 100:
-            P = P / 4.
-
-    return Y
+    return None
