295fa8b76bb7e3b89b14a3b5c28fdab5b5ebbc7e
Cumi Oyemike <cuoyemike@gmail.com>
2024-07-07 20:42:41 +0200

    fix

--- unsupervised_learning/dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Calculates the symmetric P affinities
-"""
+"""Calculates the symmetric P affinities of a data set"""
 import numpy as np
 P_init = __import__('2-P_init').P_init
 HP = __import__('3-entropy').HP
@@ -10,49 +8,48 @@ HP = __import__('3-entropy').HP
 def P_affinities(X, tol=1e-5, perplexity=30.0):
     """
     Calculates the symmetric P affinities of a data set
-    :param X: numpy.ndarray of shape (n, d) containing the dataset to be
-    transformed by t-SNE
-        n is the number of data points
-        d is the number of dimensions in each point
-    :param tol: the maximum tolerance allowed (inclusive) for the difference
-    in Shannon entropy from perplexity for all Gaussian distributions
-    :param perplexity: perplexity that all Gaussian distributions should have
-    :return: P, a numpy.ndarray of shape (n, n) containing the symmetric P
-    affinities
+    Args:
+        X: is a numpy.ndarray of shape (n, d) containing the dataset
+        to be transformed by t-SNE
+        - n is the number of data points
+        - d is the number of dimensions in each point
+        tol: is the maximum tolerance allowed (inclusive) for
+        the difference in Shannon entropy from
+        perplexity: is the perplexity that all Gaussian distributions
+        should have
+
+    Returns:
+    P, a numpy.ndarray of shape (n, n) containing the symmetric P affinities
     """
-    n, d = X.shape
+    n, _ = X.shape
     D, P, betas, H = P_init(X, perplexity)
-
-    if n == 0:
-        return P
-
     for i in range(n):
-        copy = D[i].copy()
+        copy = D[i]
         copy = np.delete(copy, i, axis=0)
         Hi, Pi = HP(copy, betas[i])
+        betamin = None
+        betamax = None
         Hdiff = Hi - H
-
-        low = None
-        high = None
 
         while np.abs(Hdiff) > tol:
             if Hdiff > 0:
-                low = betas[i, 0]
-                if high is None:
+                betamin = betas[i, 0]
+                if betamax is None:
                     betas[i] = betas[i] * 2
                 else:
-                    betas[i] = (betas[i] + high) / 2
-
+                    betas[i] = (betas[i] + betamax) / 2
             else:
-                high = betas[i, 0]
-                if low is None:
+                betamax = betas[i, 0]
+                if betamin is None:
                     betas[i] = betas[i] / 2
                 else:
-                    betas[i] = (betas[i] + low) / 2
-
+                    betas[i] = (betas[i] + betamin) / 2
+            # Recompute the values
             Hi, Pi = HP(copy, betas[i])
             Hdiff = Hi - H
-        Pi = np.insert(Pi, i, 0)
-        P[i] = Pi
-    P = (P.T + P) / (2 * n)
+        # Set the final row of P, reinserting the missing spot as 0
+        aux = np.insert(Pi, i, 0)
+        P[i] = aux
+    # The symmetrized conditional probabilities
+    P = (P.T + P) / (2*n)
     return P
